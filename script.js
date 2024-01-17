c33c2c0dab55b7e0c7aa9170c934acff51219a20
Saemi Park <imsam0124@gmail.com>
2024-01-16 23:36:18 -0800

    01/16/2024

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Shark", correct: false},
+            {text: "Shark", correct: false},
+            {text: "Shark", correct: false},
+        ]
+    }
+]
