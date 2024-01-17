d1b4429452044e9aaf94a08d50f35db1ab22b239
Saemi Park <imsam0124@gmail.com>
2024-01-16 23:49:14 -0800

    01/16/2024

--- script.js
+++ script.js
@@ -3,9 +3,50 @@ const questions = [
         question: "Which is largest animal in the world?",
         answers: [
             {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
             {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
             {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
             {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
         ]
     }
-]
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = documentgetElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
